b3f18e1156bc21ba97f5258f458dd6bf0eda820c
Yulya <75014555+YulyaS2007@users.noreply.github.com>
2021-04-29 19:28:12 +0300

    js-task_3 not ended 8 9 10

--- js/js-task_3/index.js
+++ js/js-task_3/index.js
@@ -1,1 +1,106 @@
+//1) 
+console.log("***********1.");
+let i=1;
+while (i <= 50){
+    console.log(i);
+    i++
+}
+i=35;
+while ((i <= 35) && (i>=8)){
+    console.log(i);
+    i--
+}
+
+//2) 
+console.log("***********2. в документе");
+i=89;
+while (i>=11){
+    document.write(i + "<br/>");
+    i--
+}
+
+//3) 
+console.log("***********3.");
+let a=100,
+    sum100=0;
+for (i=0; i<=a; i++){
+    sum100=sum100+i;
+}
+console.log(sum100);
+
+//4) 
+console.log("***********4.");
+a=5,
+sum=0;
+for (i=1; i<=a; i++){
+    sum=0;
+    for (let j=1; j<=i; j++){
+        sum=sum+j;
+    }
+    console.log("Sum" + i + "=" + sum);
+}
+
+//5) 
+console.log("***********5.");
+for (i=8; i<=56; i++){
+    if (i%2 != 0) continue;
+    console.log (i);
+}   
+
+//6) 
+console.log("***********6.");
+a=10;
+let mult=0;
+for (i=2; i<=a; i++){
+    for (j=2; j<=a; j++){
+    console.log(i + "*" + j + " = " + i*j);
+    } 
+}
+
+//7) 
+console.log("***********7.");
+let n=1000,
+    num=0;
+while (true){
+    if(n>=50){
+        n=n/2;
+        num++;
+    } else {
+        break;
+    }
+}
+console.log(n, " num = " + num);
+
+//8) 
+console.log("***********8.");
+
+let namberUser=prompt("Please, enter the number");
+num=0;
+sum=0;
+while (namberUser != ''  ||  namberUser != false){
+    sum = +namberUser + sum;
+    num++;  
+    namberUser=prompt("Please, enter the number");
+    if(namberUser == ''  ||  namberUser == false) {
+    break;
+    }   
+    /*while(true) {
+        isNan(namberUser)
+        console.log("Error");
+        break;
+    }  */
+}
+console.log("Общая сумма = " + sum, "Среднее арифметическое введённых чисел = " + sum/num);    
+
+//9) 
+console.log("***********9.");
+let z = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57";
+let massZ= z.split(" ");
+console.log(massZ, typeof(massZ));
+let r=Math.max(massZ);
+console.log(r);
+i= z[0];
+
+console.log(r[3])
+
 
